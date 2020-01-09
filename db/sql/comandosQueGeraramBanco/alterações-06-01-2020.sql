7ffc830f98404ce18932661468b84c0d2cc3a83b
FabioBD7 <fabio.dias@usp.br>
2020-01-09 18:58:57 -0200

    atualiza backup do banco com senha data e tel no usuario

--- db/sql/comandosQueGeraramBanco/alterações-06-01-2020.sql
+++ db/sql/comandosQueGeraramBanco/alterações-06-01-2020.sql
@@ -1,3 +1,8 @@
+//quinta-feira, 9 de janeiro de 2020
+//campo SENHA , tel  e data_inscricao na tabela usuario
+ALTER TABLE usuario ADD COLUMN senha VARCHAR(32) NOT NULL;
+   ALTER TABLE usuario ADD COLUMN tel VARCHAR(15) AFTER email;
+   ALTER TABLE usuario ADD COLUMN data_inscricao DATETIME NOT NULL; 
 
 //campo numero de passageiros na tabela embarcação 
 ALTER TABLE embarcacao ADD COLUMN num_passageiros INT(2) AFTER tamanho     ;
