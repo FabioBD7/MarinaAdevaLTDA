a9790e7fdd33889dae06a71117806fd17474dc11
FabioBD7 <fabio.dias@usp.br>
2020-01-20 06:55:07 -0300

    cliente agenda saida

--- www/cadastraSaida.php
+++ www/cadastraSaida.php
@@ -4,6 +4,8 @@ session_start();
 include_once 'banco.php';
 
 print_r($_POST);
-$data= $_POST['saidadata'] . ' ' . $_POST['saidahora'] ;
+$data= $_POST['saidadata'] . ' ' . $_POST['saidahora'] . ':00';
 echo " <br /> $data";
-$banco->query("INSERT INTO saida (barco, saida, obs) VALUE ('$_POST[barco]', '$data', '$_POST');");
+$banco->query("INSERT INTO saida (barco, saida, obs) VALUE ('$_POST[barco]', '$data', '$_POST[observacao]');");
+
+header("location: cliente.php");
